fe214c2c94441e6478ba81baee2562c9da05152b
SARTHAK CHAUHAN <sarthakchauhan@arizona.edu>
2026-09-18 19:57:06 +0530

    Remove [id] dynamic directory, consolidate to static /clients route with query param routing

--- src/app/clients/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientDetailContent from './ClientDetailContent';
-
-export function generateStaticParams() {
-  return [{ id: '_' }];
-}
-
-export default function ClientDetailPage() {
-  return <ClientDetailContent />;
-}
--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import ClientDetailContent from './[id]/ClientDetailContent';
+import ClientDetailContent from './ClientDetailContent';
 
 export default function ClientsPage() {
   return <ClientDetailContent />;
